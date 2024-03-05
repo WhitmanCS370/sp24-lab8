95233aaf408dcfb2e11c510eadedb7cd420f7ae5
Steven <steven.lin@leadvisor.net>
2024-03-05 13:58:20 -0800

    implemented sequencing backups

--- SDX-ch10/backup.py
+++ SDX-ch10/backup.py
@@ -1,3 +1,4 @@
+import os
 import csv
 import shutil
 import sys
@@ -6,12 +7,19 @@ from pathlib import Path
 
 from hash_all import hash_all
 
-
+# [file_name]
+def fileName(backup_dir):
+    for file_name in os.listdir(backup_dir):
+        if file_name.endswith(".csv") and len(file_name) == 8 and file_name.isdigit(): #condition check for manifest file
+            file_number = int(file_name.split(".")[0]) # split off the extension and convert to int
+            file_number += 1
+            return f"{file_number:08d}.csv"
 # [backup]
 def backup(source_dir, backup_dir):
     manifest = hash_all(source_dir)
     timestamp = current_time()
-    write_manifest(backup_dir, timestamp, manifest)
+    file_name = fileName(backup_dir)
+    write_manifest(backup_dir, file_name, manifest)
     copy_files(source_dir, backup_dir, manifest)
     return manifest
 # [/backup]
@@ -31,11 +39,11 @@ def current_time():
 # [/time]
 
 # [write]
-def write_manifest(backup_dir, timestamp, manifest):
+def write_manifest(backup_dir, file_name, manifest):
     backup_dir = Path(backup_dir)
     if not backup_dir.exists():
         backup_dir.mkdir()
-    manifest_file = Path(backup_dir, f"{timestamp}.csv")
+    manifest_file = Path(backup_dir, f"{file_name}.csv")
     with open(manifest_file, "w") as raw:
         writer = csv.writer(raw)
         writer.writerow(["filename", "hash"])
